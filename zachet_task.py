1d35683de40ccf6c812476bbb5bd30aaa4bf0e7e
Ilya Priemyshev <96886897+ilapri3@users.noreply.github.com>
2024-12-11 18:47:00 +0300

    Update zachet_task.py

--- zachet_task.py
+++ zachet_task.py
@@ -7,7 +7,7 @@ class Elevator:
         self.floors = 3
         self.power = 800
         self.weight = 150
-        self.elevator_position = 0
+        self.current_pos = 0
         self.speed = 0
         self.flag = 0
 
@@ -22,11 +22,12 @@ class Elevator:
         self.power = int(float(value))
 
     def start(self):
+        global current_pos, current_floor
         if self.flag == 0:
             weight = int(weight_spinbox.get())
             if weight > 0:
                 self.speed = self.power / weight
-                current_pos = self.elevator_position
+                current_pos = self.current_pos
                 current_floor = self.floors
                 dir = 1
                 if current_pos < current_floor:
@@ -35,16 +36,17 @@ class Elevator:
                     self.move_elevator(dir * -1) 
 
     def move_elevator(self, direction):
-        if direction == 1 and self.elevator_position < (self.floors - 1):
-            new_position = (self.elevator_position + 1) * (400 / self.floors)
+        if direction == 1 and current_pos < (current_floor - 1):
+            new_position = (self.current_pos + 1) * (400 / self.floors)
             for _ in range(int(400 / (self.speed / 10))): 
                 canvas.move(elevator, 0, -1 * self.speed / 10)
                 base.update()
                 base.after(10)  
             
-            self.elevator_position += 1
-            
-            if self.elevator_position < (self.floors - 1):
+            self.current_pos += 1
+            current_floor_label.config(text=f"Текущий этаж: {self.current_pos + 1}")
+
+            if self.current_pos < (self.floors - 1):
                 base.after(1000)  
             
             if self.flag == 0:
@@ -52,7 +54,8 @@ class Elevator:
 
     def update_elevator(self):
         canvas.coords(elevator, 50, 350, 150, 400)
-        self.elevator_position = 0
+        self.current_pos = 0
+        current_floor_label.config(text="Текущий этаж: 1")
 
 
 base = Tk()
@@ -79,10 +82,13 @@ weight_spinbox = ttk.Spinbox(base, from_=100, to=2000, increment = 50)
 weight_spinbox.grid(column=1, row=2)
 weight_spinbox.set(simulator.weight)
 
-start_button = ttk.Button(base, text='Старт', command=simulator.start)
+start_button = ttk.Button(base, text='Применить', command=simulator.start)
 start_button.grid(column=0, row=3)
 
 update_button = ttk.Button(base, text='Заново', command=simulator.update_elevator)
 update_button.grid(column=1, row=3)
+
+current_floor_label = ttk.Label(base, text="Текущий этаж: 1")
+current_floor_label.grid(columnspan=1, row=4)
 
 base.mainloop()
